94f3fbfba3297554ee69325451830c615cedad7c
Usama Rahman <rahmanusama390@gmail.com>
2023-10-14 19:24:53 +0600

    readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Here I Build Sortable Drag & Drop With Vanilla Javascript
--- app.js
+++ app.js
@@ -5,6 +5,17 @@ draggable.forEach(draggable => {
     draggable.addEventListener("dragstart" , () =>{
         draggable.classList.add('dragging')
     })
+    draggable.addEventListener("dragend" , () =>{
+        draggable.classList.remove('dragging')
+    })
+
+    containers.forEach(container =>{
+        container.addEventListener('dragover', e => {
+            e.preventDefault()
+            const draggable = document.querySelector('.dragging');
+            container.appendChild(draggable);
+        })
+    })
 })
 
 
