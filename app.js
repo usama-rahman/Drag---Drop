8db2ead84a5a8b144f5b5d63602ea329b2074cd6
Usama Rahman <rahmanusama390@gmail.com>
2023-10-14 19:40:01 +0600

    !

--- app.js
+++ app.js
@@ -8,14 +8,26 @@ draggable.forEach(draggable => {
     draggable.addEventListener("dragend" , () =>{
         draggable.classList.remove('dragging')
     })
+})
 
-    containers.forEach(container =>{
-        container.addEventListener('dragover', e => {
-            e.preventDefault()
-            const draggable = document.querySelector('.dragging');
-            container.appendChild(draggable);
-        })
+containers.forEach(container =>{
+    container.addEventListener('dragover', e => {
+        e.preventDefault();
+        const afterElement = getDragafterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging');
+        container.appendChild(draggable);
     })
 })
+
+    function getDragafterElement(container , y) {
+        const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+        draggableElements.reduce((closest, child) =>{
+            const box = child.getBoundingClinetRect()
+            console.log(box);
+        },{offset: Number.POSITIVE_INFINITY})
+
+    }
+
 
 
